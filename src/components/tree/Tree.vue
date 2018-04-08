885157ff0534d3987a6d671056ba19e39200b2ae
starm <starmbest@qq.com>
2018-04-08 12:10:01 +0800

    强化 Tree组件分支线主题传参

--- src/components/tree/Tree.vue
+++ src/components/tree/Tree.vue
@@ -1,6 +1,6 @@
 <!-- 树状组件组件 -->
 <template>
-	<div class="stm-tree">
+	<div class="stm-tree" :class="[theme == 1? 'theme1' : '']">
 		<!-- 左边内容 -->
 		<div class="tree-left">
 			<slot name="left"></slot>
@@ -15,6 +15,9 @@
 </template>
 <script>
 	export default{
+		props: {
+			theme:Number
+		}
 	}
 </script>
 <style lang='less'>
@@ -22,6 +25,7 @@
 	线条颜色
 */
 @linecolor: #fff;
+@linecolor1: #ccc;
 	.stm-tree{display: flex;justify-content:center;align-items: center;
 		i{margin:0 10px;}
 		div{line-height: 27px;
@@ -33,6 +37,134 @@
 				}
 				&::before{
 					background:@linecolor;
+					position: absolute;
+					content: '';
+				}
+			}
+			&:first-child{
+				*{position: relative;padding-right: 10px}
+				*:only-child{
+					&::after {
+						display: none;
+					}
+					&::before {
+						display: none;
+					}
+				}
+				*:not(:first-child):not(:last-child){
+					&::after {
+						right: -5px;
+						width: 10px;
+						height: 1px;
+						top:14px;
+					}
+					&::before {
+						right: -5px;
+						width: 1px;
+						height: 29px;
+						top:0px;
+					}
+				}
+				*:first-child{
+					&::after {
+						right: -5px;
+						width: 10px;
+						height: 1px;
+						top:14px;
+					}
+					&::before {
+						right: -5px;
+						width: 1px;
+						height: 14px;
+						top:15px;
+						text-align: right;
+					}
+				}
+				*:last-child{
+					&::after {
+						right: -5px;
+						width: 10px;
+						height: 1px;
+						bottom:14px;
+					}
+					&::before {
+						right: -5px;
+						width: 1px;
+						height: 14px;
+						bottom:15px;
+						text-align: right;
+					}
+				}
+			}
+			&:last-child{
+				*{position: relative;padding-left: 10px;
+					&:not(:first-child):not(:last-child){
+						&::after {
+							left: -5px;
+							width: 10px;
+							height: 1px;
+							top:14px;
+						}
+						&::before {
+							left: -5px;
+							width: 1px;
+							height: 29px;
+							top:0px;
+						}
+					}
+				}
+				*:only-child{
+					&::after {
+						display: none;
+					}
+					&::before {
+						display: none;
+					}
+				}
+				*:last-child{
+					&::after {
+						left: -5px;
+						width: 10px;
+						height: 1px;
+						bottom:14px;
+					}
+					&::before {
+						left: -5px;
+						width: 1px;
+						height: 14px;
+						bottom:15px;
+						text-align: right;
+					}
+				}
+				*:first-child{
+					&::after {
+						left: -5px;
+						width: 10px;
+						height: 1px;
+						top:14px;
+					}
+					&::before {
+						left: -5px;
+						width: 1px;
+						height: 14px;
+						top:15px;
+						text-align: right;
+					}
+				}
+			}
+		}
+	}
+	.stm-tree.theme1{display: flex;justify-content:center;align-items: center;
+		i{margin:0 10px;}
+		div{line-height: 27px;
+			*{position: relative;height: 29px;font-size:14px;
+				&::after{
+					background:@linecolor1;
+					position: absolute;
+					content: '';
+				}
+				&::before{
+					background:@linecolor1;
 					position: absolute;
 					content: '';
 				}
